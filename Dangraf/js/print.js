b53d38f02f550c7a68f3c810663ea729fecf15cf
TuruF <dani.cejudo18@gmail.com>
2025-06-05 20:24:15 +0200

    index + print
    He quitado el aside de subir documentos del index y lo he pasado al print.html. Mejorado el index. He mejorado el responsive del los 4 html de personalizar con su css

--- Dangraf/js/print.js
+++ Dangraf/js/print.js
@@ -47,3 +47,18 @@ let contador = 0;
 			}
 		});
 	};
+
+// Obtenemos referencias a los elementos HTML
+const inputArchivo = document.getElementById("inputDocumento"); // input para cargar archivo
+const visorPDF = document.getElementById("visorPDF");       // iframe para mostrar el PDF
+
+// Evento cuando el usuario carga un archivo PDF
+inputArchivo.addEventListener("change", function () {
+  const archivo = this.files[0]; // archivo seleccionado
+  if (archivo && archivo.type === "application/pdf") {
+    const url = URL.createObjectURL(archivo); // crea una URL temporal del archivo
+    visorPDF.src = url; // muestra el PDF en el iframe
+  } else {
+    alert("Por favor, selecciona un archivo PDF válido.");
+  }
+});
